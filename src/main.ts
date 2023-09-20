dbc3698bfaca1ea52c0847511be3aefd7fcc20de
Axel Galvier <axelgalvier0@gmail.com>
2023-09-20 14:36:56 +0200

    Added a circle for each corner (Step 2).

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,10 @@ ctx.fillRect(0, 0, width, height);
 
 ctx.fillStyle = "#ff0000";
 ctx.arc(50, 50, 50, 0, Math.PI * 2);
+ctx.moveTo(width-50, 50);
+ctx.arc(width-50, 50, 50, 0, Math.PI * 2);
+ctx.moveTo(width-50, height-50);
+ctx.arc(width-50, height-50, 50, 0, Math.PI * 2);
+ctx.moveTo(50, height-50);
+ctx.arc(50, height-50, 50, 0, Math.PI * 2);
 ctx.fill();
