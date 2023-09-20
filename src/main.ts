8c971c7de4d443a513b194c6fbc89e969d39be93
Axel Galvier <axelgalvier0@gmail.com>
2023-09-20 14:03:58 +0200

    Changed color of the circle to red (Step 1).

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ const height = (canvas.height = window.innerHeight);
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, width, height);
 
-ctx.fillStyle = "#ffffff";
+ctx.fillStyle = "#ff0000";
 ctx.arc(width / 2, height / 2, 10, 0, Math.PI * 2);
 ctx.fill();
